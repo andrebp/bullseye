0b2c102fcf942ba38bb3529617aec25146ca3c4d
André Brandão <brandao.andre21@gmail.com>
2019-10-11 21:39:42 +0100

    Add essential components to view

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -9,13 +9,70 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var alertIsVisible : Bool = false
+    @State var sliderValue: Double = 50.0
+    
+    
     var body: some View {
-        Text("Hello World")
+        VStack {
+            Spacer()
+//            Target Row
+            HStack {
+                Text("Put the bullseye as close as you can to:")
+//                var numberToGuess : Int = random()
+                Text("100")
+            }
+            Spacer()
+//            Slider Row
+            HStack{
+                Text("1")
+                Slider(value: self.$sliderValue, in: 1...100)
+                Text("100")
+            }
+            Spacer()
+//            Button Row
+            Button(action: {
+                self.alertIsVisible = true;
+                print("button pressed")
+            }) {
+                Text("Knock Knock")
+            }.alert(isPresented: $alertIsVisible) { () ->
+                Alert in return
+                Alert(title: Text("Value:"),
+                      message: Text("The slider's value is \(Int(self.sliderValue.rounded()))."),
+                      dismissButton: .default(Text("Ok!")))
+            }
+            Spacer()
+//           Score Row
+            HStack{
+                
+                Button(action: {
+                    
+                }) {
+                    Text("Start Over")
+                }
+                Spacer()
+                Text("Score:")
+                Text("0")
+                Spacer()
+                Text("Round:")
+                Text("0")
+                Spacer()
+             
+                Button(action : {
+                    
+                }) {
+                    Text("Info")
+                }
+            }.padding(.bottom, 20)
+            
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewLayout(.fixed(width: 896, height: 414))
     }
 }
